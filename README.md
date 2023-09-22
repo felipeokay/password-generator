17a9121ba2a1bc0cf263af4653c5cb7ed9681a93
unknown <kevinserrano0823@gmail.com>
2023-09-22 16:07:37 -0500

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# password-generator
+# Password-Generator
+
+## Overview
+This module consisted of Javascript and how developers can enhance a webpage beyond HTML and CSS. Javascript ultimately is needed in order for users to interact on webpages. With CSS you are doing basic styling and Javascript takes it a bit further with console log, methods, properties, etc. Specifically, in this module, I was asked to modify a starter code for users to generate a random passcode through a generator. By doing so, HTML can be linked to JS with the script.js tag. Variables were used to create a value for classes and IDs. Those classes and IDs can be targeted through Javascript with var tag and name given. To link with the actual webpage "window.prompt" can be used and within "if, if else and else" are used to give user instructions and mandatory actions. In this module I learned how to enhance a webpage with Javascript and make it more user friendly. Accessibily points can also be pointed out. 
+
+## Mock-Up
+![Alt text](image.png)
+
+## Contribute 
+kevinserrano0823@gmail.com
--- script.js
+++ script.js
@@ -6,9 +6,59 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; "abcdefghijklmnopqrstuvwxyz0123456789";
 
 }
+
+// What will be displayed to user
+var prompt = window.prompt("Choose the number of character in your password (8-128)");
+if (prompt < 8) {
+  window.alert("Your password must be at least 8 characters.")
+  return
+}
+if (prompt > 128) {
+  window.alert("Your password must be less than 128 characters.")
+  return
+}
+
+
+var promptUppercase = window.confirm("Would you like to include upper case letters in your password?")
+var promptSpecialcharacters = window.confirm("Would you like to include any special characters in your password?")
+
+
+// Creating a min and max for user experience when generating password
+function display() {
+  displayPassword.textcontent = result;
+
+}
+if (promptUppercase === true && promptSpecialcharacters === true) {
+  for (let i = 0; i < prompt; i++) {
+    var math4 = Math.floor(Math.random() * allCharacters.length);
+    result += allCharacters[math4];
+  }
+}
+else if (promptUppercase === true) {
+  for (let i = 0; i < prompt; i++) {
+    var math3 = Math.floor(Math.random() * allLetters.length);
+    result += allLetters[math3];
+  }
+}
+else if (promptSpecialCharacters === true) {
+  for (let i = 0; i < prompt; i++) {
+    var math2 = Math.floor(Math.random() * allSpecialcharacters.length);
+    result += allSpecialcharacters[math2];
+  }
+else {
+  for (let i = 0; i < prompt; i++) {
+    var math1 = Math.floor(Math.random() * lowerCase.length);
+    result += lowerCase[math1];
+  }
+}
+display()
+
+}
+
+console.log(result);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
